93e6e24b0b058633a483e0ba3e06ea322e67411b
Philipp Schulz <hadeweka@outlook.de>
2020-04-18 17:00:23 +0200

    Added dummy files

---
+++ EmulationLayer.rb
@@ -0,0 +1,49 @@
+def require(arg)
+
+	puts "Overriding required file or library: #{arg}"
+
+end
+
+class Entity
+
+end
+
+class Enemy < Entity
+
+end
+
+class Dir
+
+	def self.glob(arg)
+
+	end
+
+end
+
+module Gosu
+
+	class Color
+
+	end
+
+	class Image
+
+	end
+
+	class Sample
+
+	end
+
+	class Song
+
+	end
+
+	class TextInput
+
+	end
+	
+	class Window
+
+	end
+
+end
